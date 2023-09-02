4d2dea58cca89f24179a0c75a4901087c8adf0f3
Aleksander <olek.morawski@gmail.com>
2023-09-03 00:16:47 +0200

    dupa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom'; 
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleCreateNowClick = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (accounts.length > 0) {
+          navigate('/form/'); 
+        }
+      } catch (error) {
+        console.error('User denied account access');
+      }
+    } else {
+      alert('MetaMask is not installed. Please consider installing it: https://metamask.io/');
+    }
+  };
+
   return (
     <>
       <div className="overlay">
@@ -10,7 +28,6 @@ const Home = () => {
           <div className="main_box">
             <div className="under_text">
               <p className="p1">CREATE UNIQUE</p>
-
               <p className="p2">&nbsp;NFTS</p>
             </div>
 
@@ -27,14 +44,14 @@ const Home = () => {
               />
             </div>
 
-            <div className="btn_und">
+            <div className="btn_und" onClick={handleCreateNowClick}>
               <p className="btn_text_und">✨ Create Now ✨</p>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
